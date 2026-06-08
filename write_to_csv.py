43063d08ff2229e97d51e00d5a09d54ae2be1982
nicholastyner <129696089+nicholastyner@users.noreply.github.com>
2026-06-07 22:41:27 -0500

    CSV comma update

--- write_to_csv.py
+++ write_to_csv.py
@@ -1,17 +1,23 @@
 def csv_headers(weather_dict):
-    with open("output.csv", "w") as file:
-        for midpoint in weather_dict.keys():
+    headers = []
+    for midpoint in weather_dict.keys():
             if midpoint == "general":
                 for key in weather_dict[midpoint].keys():
-                    file.write(f"{key}")
+                    headers.append(key)
             else:
                 for key in weather_dict[midpoint].keys():
-                    file.write(f"Point {midpoint}: {key},")
-        file.write("\n")
+                    headers.append(f"Point {midpoint}: {key}")
+        
+    with open("output.csv", "w") as file:
+        file.write(",".join(headers) + "\n")
+        
     
 def to_csv(weather_dict):
-    with open("output.csv", "a") as file:
-        for point in weather_dict.values():
+    data = []
+    
+    for point in weather_dict.values():
             for value in point.values():
-                file.write(f"{value},")
-        file.write("\n")
+                data.append(str(value))
+    
+    with open("output.csv", "a") as file:
+        file.write(",".join(data) + "\n")
